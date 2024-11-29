372c0b59287cb4ba8308407ab6c504cad29b2c57
chanwl19 <131731524+chanwl19@users.noreply.github.com>
2024-11-29 09:36:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,81 @@
 import os
+import re
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
-from transformers import BartForConditionalGeneration, BartTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer
-#from datasets import load_dataset
+from fastapi.middleware.cors import CORSMiddleware
+from transformers import BartForConditionalGeneration, BartTokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.text_rank import TextRankSummarizer
 
 # Create FastAPI app
 app = FastAPI()
 
-model_path = "./model"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"]
+)
+
 model_name = "facebook/bart-large-xsum"
 model = BartForConditionalGeneration.from_pretrained(model_name)
-tokenizer = BartTokenizer.from_pretrained(model_name)
-
-def check_model_exists():
-    #print(os.path.exists(model_path))
-    return True
-
-""" def train_model():
-
-    print('Start to train model....')
-    dataset = load_dataset("xsum", trust_remote_code=True)
-    processed_dataset = dataset.map(preprocess_data, batched=True)
-
-    training_args = Seq2SeqTrainingArguments(
-        output_dir=model_path,  # Directory to save the model
-        evaluation_strategy="epoch",
-        learning_rate=2e-5,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        weight_decay=0.01,
-        save_total_limit=3,
-        num_train_epochs=3,
-        predict_with_generate=True,  # Enables summary generation during evaluation
-        logging_dir="./logs",
-    )
-
-    trainer = Seq2SeqTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=processed_dataset["train"],
-        eval_dataset=processed_dataset["validation"],
-        tokenizer=tokenizer,
-    )
-
-    trainer.train()
-    result = trainer.evaluation()
-    print('evaluate result ' + result)
-    model.save_pretrained(model_path)
-    tokenizer.save_pretrained(model_path)
-    is_trained = True
-    print('Finish to train model....')
-
-def preprocess_data(batch):
-    inputs = tokenizer(
-        batch["document"], 
-        max_length=1024, 
-        truncation=True, 
-        padding="max_length", 
-        return_tensors="pt"
-    )
-    targets = tokenizer(
-        batch["summary"], 
-        max_length=128, 
-        truncation=True, 
-        padding="max_length", 
-        return_tensors="pt"
-    )
-    return {"input_ids": inputs["input_ids"], "attention_mask": inputs["attention_mask"], "labels": targets["input_ids"]} """
-
-def train_model():
-    print('trained')
-
-if not check_model_exists():
-    train_model()
-else :
-    print('Model trained')
+bart_tokenizer = BartTokenizer.from_pretrained(model_name)
 
 # Define the route for summarization
 @app.post("/summarize")
 async def summarize(request: Request):
+
     body = await request.json()
     text = body.get('text')
     max_word = body.get('max', 250)
-    model = BartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = BartTokenizer.from_pretrained(model_name)
 
-    if not check_model_exists(): # should be true
-        model = BartForConditionalGeneration.from_pretrained(model_path)
-        tokenizer = BartTokenizer.from_pretrained(model_path)
+    #Get abstractive summary
+    abs_summary = get_abstractive(text, max_word)
+    ext_summary = get_extractive(text, max_word)
 
-    print('start summarizing...')
-    # Tokenize input text and generate summary
-    inputs = tokenizer.encode("summarize: " + text, max_length=1024, return_tensors="pt", truncation=True)
+    abs_new_words = compare_paragraphs(text, abs_summary)
+    ext_new_words = compare_paragraphs(text, ext_summary)
+
+    return {"abs_summary": abs_summary, 'ext_summary': ext_summary, 'abs_new_words' : abs_new_words , 'ext_new_words': ext_new_words}
+
+def get_abstractive(text: str, max_len: int) -> str:
+    print('start generating abstractive summary...')
+    inputs = bart_tokenizer.encode("summarize: " + text, max_length=1024, return_tensors="pt", truncation=True)
     print('finished tokenizing...')
-    # Generate summary using BART
+
+    print('start to use BART model to generate abstractive summary...')
     summary_ids = model.generate(inputs, 
-                                 max_length=max_word, 
+                                 max_length=max_len, 
                                  min_length=50,
                                  num_beams=8, 
                                  length_penalty=2.5,
                                  early_stopping=True)
 
-    
-    print('finished generate...')
-    # Decode summary
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    
-    print('finished summary... ' + summary)
-    return {"summary": summary}
+    summary = bart_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    print('finished generate abstractive summary...')
+    return summary
 
+def get_extractive(text: str, max_len: int) -> str:
+    print('start generating extractive summary...')
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = TextRankSummarizer()
+    
+    print('start to generate summary sentences...')
+    summary_sentences = summarizer(parser.document, 10) 
+    summary = " ".join([str(sentence) for sentence in summary_sentences])
+    
+    summary_words = summary.split()
+    if len(summary_words) > max_len:
+        summary = " ".join(summary_words[:max_len   ])
+    
+    return summary
+
+def compare_paragraphs(paragraph1:str, paragraph2:str) -> list[str]:
+
+    words1 = set(re.findall(r'\b\w+\b', paragraph1.lower()))
+    words2 = set(re.findall(r'\b\w+\b', paragraph2.lower()))
+    
+    diff2 = words2 - words1
+    
+    return list(diff2)
